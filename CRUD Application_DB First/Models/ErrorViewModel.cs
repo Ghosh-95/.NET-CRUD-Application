82f4d5ddc48329248b87ea7ed20780af38d91b57
Sushovan <sushovanghosh960@gmail.com>
2025-04-08 11:36:02 +0530

    project setup

---
+++ CRUD Application_DB First/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUD_Application_DB_First.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
